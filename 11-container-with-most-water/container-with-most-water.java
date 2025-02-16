44145395f8b85be31fbeaed5a3ca95cf3b4ed227
Dileep Kumar Ramabrki <142863170+DileepKumarRambarki@users.noreply.github.com>
2025-02-16 09:12:48 +0530

    Time: 4 ms (90.59%) | Memory: 57.4 MB (83.99%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -4,7 +4,16 @@ class Solution {
         int n=height.length;
         int i=0,j=n-1;
         while(i<j){
-            water=Math.max(water,(j-i)*Math.min(height[i],height[j]));
+            int sum=0;
+            if(height[i]<height[j]){
+                sum=(j-i)*height[i];
+            }
+            else{
+                sum=(j-i)*height[j];
+            }
+            if(sum>water){
+                water=sum;
+            }
             if(height[i]<height[j]){
                 i++;
             }
