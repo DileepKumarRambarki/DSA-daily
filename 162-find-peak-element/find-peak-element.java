ff93e3139bb3b44fdf000475474c1a73fd2dfc40
Dileep Kumar Ramabrki <142863170+DileepKumarRambarki@users.noreply.github.com>
2025-02-23 08:45:49 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (54.49%) - LeetSync

---
+++ 162-find-peak-element/find-peak-element.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        if(nums.length==1) return 0;
+        int low=0,high=nums.length-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(mid==0&&nums[mid]>nums[mid+1]){
+                return mid;
+            }
+            else if(mid==0&&nums[mid]<nums[mid+1]){
+                low=mid+1;
+                continue;
+            }
+            else if(mid==nums.length-1&&nums[mid]>nums[mid-1]){
+                return mid;
+            }
+            else if(mid==nums.length-1&&nums[mid]<nums[mid-1]) {low=mid-1;continue;}
+            else if(nums[mid]>nums[mid-1]&&nums[mid]>nums[mid+1]){
+                return mid;
+            }
+            else if(nums[mid]<nums[mid+1]){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return -1;
+    }
+}
