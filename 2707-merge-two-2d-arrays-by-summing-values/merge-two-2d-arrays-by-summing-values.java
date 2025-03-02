c98762d8959e445014bc28dbb5b78ccf16ea7b9e
Dileep Kumar Ramabrki <142863170+DileepKumarRambarki@users.noreply.github.com>
2025-03-02 19:06:13 +0530

    Time: 1 ms (86.98%) | Memory: 45.3 MB (73.05%) - LeetSync

--- 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.java
+++ 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.java
@@ -3,31 +3,37 @@ class Solution {
         int n1=nums1.length;
         int n2=nums2.length;
         int i=0,j=0;
-        LinkedHashMap<Integer,Integer> hm=new LinkedHashMap<>();
+        List<int[]> l=new ArrayList<>();
         while(i<n1&&j<n2){
             if(nums1[i][0]<nums2[j][0]){
-                hm.put(nums1[i][0],hm.getOrDefault(nums1[i][0],0)+nums1[i][1]);
+                l.add(new int[]{nums1[i][0],nums1[i][1]});
                 i++;
             }
+            else if(nums1[i][0]>nums2[j][0]){
+                l.add(new int[]{nums2[j][0],nums2[j][1]});
+                j++;
+            }
             else{
-                hm.put(nums2[j][0],hm.getOrDefault(nums2[j][0],0)+nums2[j][1]);
+                l.add(new int[]{nums1[i][0],nums1[i][1]+nums2[j][1]});
+                i++;
                 j++;
             }
         }
         while(i<n1){
-             hm.put(nums1[i][0],hm.getOrDefault(nums1[i][0],0)+nums1[i][1]);
+
+                l.add(new int[]{nums1[i][0],nums1[i][1]});
                 i++;
         }
         while(j<n2){
-            hm.put(nums2[j][0],hm.getOrDefault(nums2[j][0],0)+nums2[j][1]);
+                l.add(new int[]{nums2[j][0],nums2[j][1]});
                 j++;
 
         }
-        int res[][]=new int[hm.size()][2];
+        int res[][]=new int[l.size()][2];
         int k=0;
-        for(int id:hm.keySet()){
-            res[k][0]=id;
-            res[k][1]=hm.get(id);
+        for(int[] id:l){
+            res[k][0]=id[0];
+            res[k][1]=id[1];
             k++;
         }
         return res;
