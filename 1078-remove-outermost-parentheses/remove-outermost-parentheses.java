1a7fd0774b199116cb767ef0fc712a834d78fc70
Dileep Kumar Ramabrki <142863170+DileepKumarRambarki@users.noreply.github.com>
2025-03-03 20:00:48 +0530

    Time: 7 ms (33.47%) | Memory: 45.6 MB (5.88%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
@@ -3,22 +3,25 @@ class Solution {
         Stack<Character> st=new Stack<>();
         String res="";
         int first=0;
-        st.push('(');
-        for(int i=1;i<s.length();i++){
+        int left=0;
+        int right=0;
+        for(int i=0;i<s.length();i++){
             char ch=s.charAt(i);
-            if(ch=='('){
-                st.push(ch);
-            }
-            else{
-                if(i>first && (i-first)%2==1&&st.size()==1){
-                String str="";
-                    str=s.substring(first,i+1);
-                    first=i+1;
-                str=str.substring(1,str.length()-1);
-                res=res+str;
+                if(ch=='('){
+                    left++;
                 }
-                 char pp=st.pop();
-            }
+                else{
+                    right++;
+                }
+                if(left==right){
+                    String str=s.substring(first,i+1);
+                    str=str.substring(1,str.length()-1);
+                    res+=str;
+                    first=i+1;
+                    left=0;
+                    right=0;
+                }
+            
         }
         return res;
     }
