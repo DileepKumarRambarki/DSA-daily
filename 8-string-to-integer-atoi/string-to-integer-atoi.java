7b37ce5df307f6ec1f88f3013843f6c767bb658c
Dileep Kumar Ramabrki <142863170+DileepKumarRambarki@users.noreply.github.com>
2025-03-05 19:17:45 +0530

    Time: 7 ms (7.08%) | Memory: 43.2 MB (7.42%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -5,11 +5,13 @@ class Solution {
         String num=new String();
         int i=0,j=0;
         if(s.length()==0) return 0;
+        int sign=0;
         if(s.charAt(0)=='+'){
+            sign=0;
             j++;
         }
         else if(s.charAt(0)=='-'){
-            num+='-';
+            sign=1;
             j++;
         }
         int dig=0;
@@ -26,14 +28,17 @@ class Solution {
         }
         if(num.length()==0) return 0;
         if(dig==0) return 0;
-        BigInteger l=new BigInteger(num);
-        
-        if(l.compareTo(BigInteger.valueOf(Integer.MAX_VALUE))>0){
-            return Integer.MAX_VALUE;
+        long res=0;
+        for(int k=0;k<num.length();k++){
+            int digit=num.charAt(k)-'0';
+            res=(res*10)+digit;
+            if(sign==0&& res>Integer.MAX_VALUE){
+                return Integer.MAX_VALUE;
+            }
+            if(sign==1&& -res<Integer.MIN_VALUE){
+                return Integer.MIN_VALUE;
+            }
         }
-        else if(l.compareTo(BigInteger.valueOf(Integer.MIN_VALUE))<0){
-            return Integer.MIN_VALUE;
-        }
-        return l.intValue();
+        return sign==0?(int)res:-(int)res;
     }
 }
