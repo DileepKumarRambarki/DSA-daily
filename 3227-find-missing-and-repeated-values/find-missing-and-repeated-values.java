65ff9e70bb7ca4a95911c2bd9f4034c686f802c2
Dileep Kumar Ramabrki <142863170+DileepKumarRambarki@users.noreply.github.com>
2025-03-06 22:53:35 +0530

    Time: 1 ms (100.00%) | Memory: 45.5 MB (80.11%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.java
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.java
@@ -2,24 +2,20 @@ class Solution {
     public int[] findMissingAndRepeatedValues(int[][] grid) {
         int xor=0;
         int k=0;
+        long sum=0,sqsum=0;
+        int n=grid.length*grid.length;
+        long ogsum=(long)n*(n+1)/2;
+       long  ogsqsum=(long)n*(n+1)*(2*n+1)/6;
         for(int i=0;i<grid.length;i++){
             for(int j=0;j<grid.length;j++){
-                xor^=grid[i][j];
-                k++;
-                xor^=k;
+                sum+=grid[i][j];
+                sqsum+=grid[i][j]*grid[i][j];
             }
         }
-        HashMap<Integer,Integer> hm=new HashMap<>();
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid.length;j++){
-               if(hm.containsKey(grid[i][j])){
-                return new int[]{grid[i][j],xor^grid[i][j]};
-               }
-               else{
-                hm.put(grid[i][j],1);
-               }
-            }
-        }
-        return new int[]{-1,-1};
+        long amb=ogsum-sum;
+        long apb=(ogsqsum-sqsum)/amb;
+        int a=(int)((amb+apb)/2);
+        int b=(int)(apb-a);
+        return new int[]{b,a};
     }
 }
