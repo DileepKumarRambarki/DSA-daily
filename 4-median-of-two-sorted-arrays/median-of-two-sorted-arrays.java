53b60587528448b38f6a64a167e9cd594ac684f2
Dileep Kumar Ramabrki <142863170+DileepKumarRambarki@users.noreply.github.com>
2025-03-08 16:01:41 +0530

    Time: 1 ms (100.00%) | Memory: 45.8 MB (96.74%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -3,35 +3,61 @@ class Solution {
         int n1=nums1.length;
         int n2=nums2.length;
         int n=n1+n2;
-        int res[]=new int[n];
         int a=-1,b=-1;
-       
+       if(n%2==0){
+        a=n/2-1;
+        b=n/2;
+       }
+       else{
+        a=n/2;
+       }
         int i=0,j=0;
-        int count=0;
+        int count=-1;
+        int val=-1;
+        int va=-1,vb=-1;
         while(i<n1&&j<n2){
             if(nums1[i]<nums2[j]){
-                res[count++]=nums1[i];
+                count++;
+                val=nums1[i];
                 i++;
             }
             else{
-                res[count++]=nums2[j];
+                count++;
+                val=nums2[j];
                 j++;
             }
-
+            if(count==a){
+                va=val;
+            }
+            if(count==b){
+                vb=val;
+                return (va+vb)/2.0;
+            }
         }
         while(i<n1){
-            res[count++]=nums1[i];
+            count++;
+            val=nums1[i];
             i++;
+             if(count==a){
+                va=val;
+            }
+            if(count==b){
+                vb=val;
+                return (va+vb)/2.0;
+            }
         }
         while(j<n2){
-            res[count++]=nums2[j];
+            count++;
+            val=nums2[j];
             j++;
+             if(count==a){
+                va=val;
+            }
+            if(count==b){
+                vb=val;
+                return (va+vb)/2.0;
+            }
         }
-        if(n%2==0){
-            a=n/2-1;
-            b=n/2;
-            return (res[a]+res[b])/2.0;
-        }
-        return res[n/2];
+        return va;
     }
 }
