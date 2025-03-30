af22e7dcf471753a3bd63800f5da6170738e44b4
Dileep Kumar Ramabrki <142863170+DileepKumarRambarki@users.noreply.github.com>
2025-03-30 22:49:58 +0530

    Time: 0 ms (100.00%) | Memory: 44.9 MB (19.19%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
@@ -16,22 +16,48 @@ class Node {
 class Solution {
     public Node copyRandomList(Node head) {
         if(head==null) return null;
-        Node old=head;
-        Node dummy=new Node(-1);
-        Node copy=dummy;
-        HashMap<Node,Node> hm=new HashMap<>();
+        // Node old=head;
+        // Node dummy=new Node(-1);
+        // Node copy=dummy;
+        // HashMap<Node,Node> hm=new HashMap<>();
+        // while(old!=null){
+        //     Node a=new Node(old.val);
+        //     copy.next=a;
+        //     copy =a;
+        //     hm.put(old,a);
+        //     old=old.next;
+        // }
+        // old=head;
+        // copy=dummy.next;
+        // while(copy!=null){
+        //     copy.random=hm.get(old.random);
+        //     copy=copy.next;
+        //     old=old.next;
+        // }
+        // return dummy.next;
+        Node old=head; 
         while(old!=null){
             Node a=new Node(old.val);
-            copy.next=a;
-            copy =a;
-            hm.put(old,a);
-            old=old.next;
+            a.random=old.random;
+            a.next=old.next;
+            old.next=a;
+            old=a.next;
+        }
+        Node temp=head.next;
+        while(temp!=null){
+            if(temp.random!=null){
+                temp.random=temp.random.next;
+            }
+            if(temp.next==null) break;
+            temp=temp.next.next;
         }
         old=head;
-        copy=dummy.next;
-        while(copy!=null){
-            copy.random=hm.get(old.random);
+        Node dummy=new Node(-1);
+        Node copy=dummy;
+        while(old!=null){
+            copy.next=old.next;
             copy=copy.next;
+            old.next=old.next.next;
             old=old.next;
         }
         return dummy.next;
