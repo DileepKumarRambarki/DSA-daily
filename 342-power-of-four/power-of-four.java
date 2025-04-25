ba0dd583a1594621ef285c37d209064b1502a1c8
Dileep Kumar Ramabrki <142863170+DileepKumarRambarki@users.noreply.github.com>
2025-04-25 17:24:41 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (9.08%) - LeetSync

---
+++ 342-power-of-four/power-of-four.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if(n==0) return false;
+        if(n==1) return true;
+        if(n%4!=0) return false;
+        if(n%16==0) return isPowerOfFour(n/16);
+        return isPowerOfFour(n/4);
+    }
+
+}
