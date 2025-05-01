bd5a82b2320aa7d06aa453f846d862a22cd05ec2
Dileep Kumar Ramabrki <142863170+DileepKumarRambarki@users.noreply.github.com>
2025-05-01 14:53:21 +0530

    Time: 1 ms (51.56%) | Memory: 42.7 MB (65.40%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,22 +1,39 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
+        // recursive solution
+        // List<List<Integer>> res=new ArrayList<>();
+        // for(int i=0;i<nums.length;i++){
+        //     List<Integer> l=new ArrayList<>();
+        //     l.add(nums[i]);
+        //     res.add(l);
+        //     merge(l,res,nums,i);
+        // }
+        // res.add(new ArrayList<>());
+        // return res;
+
+        // bit wise manuplation
         List<List<Integer>> res=new ArrayList<>();
-        for(int i=0;i<nums.length;i++){
+        int n=(int)Math.pow(2,nums.length);
+        for(int i=0;i<n;i++){
             List<Integer> l=new ArrayList<>();
-            l.add(nums[i]);
+            int temp=i;
+            for(int j=0;j<nums.length;j++){
+                if((temp&1)==1){
+                    l.add(nums[j]);
+                }
+                temp=temp>>1;
+            }
             res.add(l);
-            merge(l,res,nums,i);
         }
-        res.add(new ArrayList<>());
         return res;
     }
-    void merge(List<Integer> l, List<List<Integer>> res,int nums[],int right){
-        if(right>=nums.length) return;
-        for(int i=right+1;i<nums.length;i++){
-            List<Integer> temp=new ArrayList<>(l);
-            temp.add(nums[i]);
-            res.add(temp);
-            merge(temp,res,nums,i);
-        }
-    }
+    // void merge(List<Integer> l, List<List<Integer>> res,int nums[],int right){
+    //     if(right>=nums.length) return;
+    //     for(int i=right+1;i<nums.length;i++){
+    //         List<Integer> temp=new ArrayList<>(l);
+    //         temp.add(nums[i]);
+    //         res.add(temp);
+    //         merge(temp,res,nums,i);
+    //     }
+    // }
 }
