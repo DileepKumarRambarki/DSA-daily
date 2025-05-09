70a1bc3b27ec115665b8f1fb431d43e5aec79386
Dileep Kumar Ramabrki <142863170+DileepKumarRambarki@users.noreply.github.com>
2025-05-09 15:02:20 +0530

    Time: 1 ms (55.08%) | Memory: 42.3 MB (51.24%) - LeetSync

---
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -0,0 +1,33 @@
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        List<String> res=new ArrayList<>();
+        if(digits.length()==0){
+            return res;
+        }
+        HashMap<Character,char[]> keys=new HashMap<>();
+        keys.put('2',new char[]{'a','b','c'});
+        keys.put('3',new char[]{'d','e','f'});
+        keys.put('4',new char[]{'g','h','i'});
+        keys.put('5',new char[]{'j','k','l'});
+        keys.put('6',new char[]{'m','n','o'});
+        keys.put('7',new char[]{'p','q','r','s'});
+        keys.put('8',new char[]{'t','u','v'});
+        keys.put('9',new char[]{'w','x','y','z'});
+        StringBuilder sb=new StringBuilder();
+        rec(sb,res,0,digits,keys);
+        return res;
+    }
+    void rec(StringBuilder sb, List<String> res,int pos,String digits, HashMap<Character,char[]> keys){
+        if(pos==digits.length()){
+            res.add(sb.toString());
+            return;
+        }
+        char ch=digits.charAt(pos);
+        char arr[]=keys.get(ch);
+        for(char a:arr){
+            StringBuilder temp=new StringBuilder(sb);
+            temp.append(a);
+            rec(temp,res,pos+1,digits,keys);
+        }
+    }
+}
