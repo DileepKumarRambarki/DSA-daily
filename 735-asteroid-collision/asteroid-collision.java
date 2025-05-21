d1b2d415b033d271cd6385466e9655cc7bc3359a
Dileep Kumar Ramabrki <142863170+DileepKumarRambarki@users.noreply.github.com>
2025-05-21 11:52:07 +0530

    Time: 4 ms (94.80%) | Memory: 45.5 MB (34.95%) - LeetSync

---
+++ 735-asteroid-collision/asteroid-collision.java
@@ -0,0 +1,53 @@
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> st=new Stack<>();
+        for(int i=0;i<asteroids.length;i++){
+            int curr=asteroids[i];
+            help(curr,st);
+        }
+       int res[]=new int[st.size()];
+       int k=st.size()-1;
+       while(!st.isEmpty()){
+        res[k--]=st.pop();
+       }
+       return res;
+    }
+    void help(int curr,Stack<Integer> st){
+        if(st.isEmpty()){
+            st.push(curr);
+            return;
+        }
+        int peek=st.peek();
+        if((curr>=0&&peek>=0)||(curr<0&&peek<0)){
+            st.push(curr);
+            return;
+        }
+        else if((curr<0&&peek>0)){
+            if(-curr>peek){
+                st.pop();
+                help(curr,st);
+            }
+            else if(-curr<peek){
+                return;
+            }
+            else{
+                st.pop();
+                return;
+            }
+        }
+        else if((curr>0)){
+            // if(-peek>curr){
+            //     return;
+            // }
+            // else if(-peek<curr){
+            //     st.pop();
+            //     help(curr,st);
+            // }
+            // else{
+            //     st.pop();
+            //     return;
+            // }
+            st.push(curr);
+        }
+    }
+}
