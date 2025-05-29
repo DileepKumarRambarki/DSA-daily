c2ed09dfdde5587fc96b6237bb95ed05595fcdd3
Dileep Kumar Ramabrki <142863170+DileepKumarRambarki@users.noreply.github.com>
2025-05-29 17:18:14 +0530

    Time: 8 ms (61.93%) | Memory: 43 MB (51.80%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,30 +1,21 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        int freq1[]=new int[26];
+        if(s1.length()>s2.length()) return false;
+        int f1[]=new int[26];
+        int f2[]=new int[26];
         for(int i=0;i<s1.length();i++){
-            freq1[s1.charAt(i)-'a']++;
+            f1[s1.charAt(i)-'a']++;
+            f2[s2.charAt(i)-'a']++;
         }
-        int l=0,r=0;
+        int r=s1.length()-1,l=0;
         while(r<s2.length()){
-            char ch=s2.charAt(r);
-            if(freq1[ch-'a']>0){
-                int freq2[]=freq1.clone();
-                int j=r;
-                int count=0;
-                if(s2.length()-r<s1.length()) return false;
-                while(j-r<s1.length()){
-                    if(freq2[s2.charAt(j)-'a']>0){
-                        freq2[s2.charAt(j)-'a']--;
-                        count++;
-                    }
-                    else{
-                        break;
-                    }
-                    j++;
-                }
-                if(count==s1.length()) return true;
-            }
+            if(Arrays.equals(f1,f2)) return true;            
             r++;
+            if(r<s2.length()){
+                f2[s2.charAt(r)-'a']++;
+            }
+            f2[s2.charAt(l)-'a']--;
+            l++;
         }
         return false;
     }
